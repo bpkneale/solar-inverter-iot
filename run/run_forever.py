c2d2cc2ef74b2fb993952ae74b0b5ada1b58d3ee
Ben Kneale <bpkneale@gmail.com>
2019-06-29 14:57:51 +0800

    Added docker

--- run/run_forever.py
+++ run/run_forever.py
@@ -3,12 +3,17 @@ from inverter.fronius import FroniusInverterApi
 from monitor.publisher import Publisher
 import time
 import logging
+import argparse
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
+    parser = argparse.ArgumentParser(description="Solar Inverter IoT client")
+    parser.add_argument("--secrets", dest='secrets', type=str, help='Path to client secrets (certs, config, etc)',
+                        default='secrets')
+    args = parser.parse_args()
     api = FroniusInverterApi("http://192.168.1.111")
-    pub = Publisher()
+    pub = Publisher(args.secrets)
     monitor = InverterMonitor(api, pub)
     monitor.start()
     try:
